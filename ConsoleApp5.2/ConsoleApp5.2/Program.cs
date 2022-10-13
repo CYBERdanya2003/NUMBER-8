d2c7bf690ff318f6ef05d09f3c12fcbc6db30764
CYBERdanya2003 <91051801+CYBERdanya2003@users.noreply.github.com>
2022-10-13 20:34:32 +0500

    5.2

---
+++ ConsoleApp5.2/ConsoleApp5.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string tiara = "клавиатура";
+        string v = tiara.Substring(6, 1) + tiara.Substring(4, 2) + tiara.Substring(8, 2);
+        Console.WriteLine(v);
+
+        string ruka = "клавиатура";
+        string V = ruka.Substring(8, 1) + ruka.Substring(7, 1) + ruka.Substring(0, 1) + ruka.Substring(2, 1);
+        Console.WriteLine(V);
+
+        Console.ReadKey();
+    }
+}
