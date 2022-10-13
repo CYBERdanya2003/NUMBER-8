52fe8270ccf1be20c949096a5398dad40efd1ec9
CYBERdanya2003 <91051801+CYBERdanya2003@users.noreply.github.com>
2022-10-13 20:39:59 +0500

    7

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите X");
+        Console.WriteLine("---------");
+        var x = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Значение функции при данном X: " + F(x));
+        Console.ReadLine();
+    }
+    static double F(int x)
+    {
+        if (x < -1)
+            return 1;
+        else if (x > 1)
+            return -1;
+        else
+            return -x;
+    }
+}
