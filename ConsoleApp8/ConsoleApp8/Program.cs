782b025dc8bf7cf571c61a0c2f8688c962496b12
CYBERdanya2003 <91051801+CYBERdanya2003@users.noreply.github.com>
2022-10-13 20:18:44 +0500

    tsk8

--- ConsoleApp8/ConsoleApp8/Program.cs
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Res = 0;
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i <= n; i++)
+        {
+            Res = Res + (n + i) * (n + i);
+        }
+        Console.WriteLine(Res);
+        Console.ReadKey();
+    }
+
+}
