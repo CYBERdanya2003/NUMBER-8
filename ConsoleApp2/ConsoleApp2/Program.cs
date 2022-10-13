b838c84d867f5f9b04ac77ea13d2b6243171280a
CYBERdanya2003 <91051801+CYBERdanya2003@users.noreply.github.com>
2022-10-13 20:24:48 +0500

    Update Program.cs

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите d внешнего кольца");
+        Console.WriteLine("-------------------------");
+        double D = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("Введите d внутреннего кольца");
+        Console.WriteLine("----------------------------");
+        double d = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("----------------------------");
+        double PI = Math.PI;
+
+        Console.WriteLine($"S кольца = " + (PI / 4 * (D * D - d * d)));
+        Console.Read();
+    }
+}
