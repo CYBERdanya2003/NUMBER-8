814bda2d8a9378ee945938dfb7f0579b97b27001
CYBERdanya2003 <91051801+CYBERdanya2003@users.noreply.github.com>
2022-10-19 19:10:20 +0500

    N4

---
+++ N4/N4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+// ДЗ №4
+// 
+//X = SIN (1 + SIN (2 + SIN (3 + SIN 4)))
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double X; Console.WriteLine(Math.Round(GoToG(), 3)); Console.ReadKey();
+    }
+    static double GoToG()
+    {
+        double X;
+        double s4 = 3 + Math.Sin(4);
+        double s3 = 2 + Math.Sin(s4);
+        double s2 = 1 + Math.Sin(s3);
+        double s = Math.Sin(s2);
+        return X = s;
+    }
+
+}
